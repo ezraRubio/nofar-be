9560bdca5949437d0a14ec66abee036326899834
Ezra <ezra@ezrarubio.com>
2024-01-23 13:05:45 +0200

    implemented email processor

---
+++ src/email/email.processor.ts
@@ -0,0 +1,49 @@
+import { MailGateway } from "@/utils/mailer/gateway";
+import { Attachment, Email } from "./email.model";
+import { Mailer } from "@/utils/mailer/mailer";
+
+export class EmailProcessor {
+  private _sender: string;
+  private _gateway: MailGateway;
+
+  constructor(sender: string, gateway: MailGateway) {
+    this._sender = sender;
+    this._gateway = gateway;
+  }
+
+  //TODO: getters for dif mailers
+  private static getBasicMailer(): Mailer {
+    return new Mailer(__dirname + "/mailers/main.ejs");
+  }
+
+  async sendMail(
+    receivers: string[],
+    email: Email,
+    layout: Mailer,
+    attachment?: Attachment
+  ) {
+    const mailer = EmailProcessor.getBasicMailer();
+    const html = await layout.toHtmlString(undefined, {
+      receivers,
+      templatePath: mailer.path,
+      ...mailer.attachmentsObject,
+    });
+
+    const attachments = [...layout.attachments, ...mailer.attachments];
+    if (attachment)
+      attachments.push({
+        filename: attachment.fileName,
+        content: attachment.content,
+      });
+      
+    const subject = "an email subject";
+
+    await this._gateway.sendMail(
+      this._sender,
+      receivers.join(", "),
+      subject,
+      html,
+      attachments
+    );
+  }
+}
